c6eeadc53ff27181a7f7d8099f4e7f2bed1db11a
misobae <misobae0731@gmail.com>
2024-03-08 23:26:45 +0900

    feat: path params, query string 수정

--- src/api/weatherApi.ts
+++ src/api/weatherApi.ts
@@ -1,19 +1,18 @@
 import axios from "axios";
+import { Location } from "../types/LocationTypes";
 
 const BASE_URL = `https://api.openweathermap.org/data/2.5`;
 const API_KEY = process.env.REACT_APP_OPENWEATHER_API_KEY;
 
-export interface Location {
-  lat: number;
-  lon: number;
-}
 
+// 사용자 위치 정보로 오늘 날씨 가져오기
 export async function fetchWeatherByLocation(location: Location | undefined) {
   if (location && typeof location === "object") {
     const { lat, lon } = location;
 
     try {
-      const response = await axios.get(`${BASE_URL}/weather?lat=${lat}&lon=${lon}&exclude=minutely,hourly&units=metric&appid=${API_KEY}`);
+      const response = await axios.get(`${BASE_URL}/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly&units=metric&appid=${API_KEY}`);
+
       return response.data;
     } catch (error) {
       console.error('날씨 데이터를 가져오는 중 에러 발생:', error);
@@ -23,15 +22,16 @@ export async function fetchWeatherByLocation(location: Location | undefined) {
     console.log('위치 정보를 가져올 수 없습니다.');
     throw new Error('위치 정보를 가져올 수 없습니다.');
   }
-}
+};
 
+// 검색된 도시 이름으로 오늘 날씨 가져오기
 export async function fetchWeatherByCity(cityName: string) {
   try {
     const response = await axios.get(`${BASE_URL}/weather?q=${cityName}&exclude=minutely,hourly&units=metric&appid=${API_KEY}`);
-    console.log("city Weather", response.data)
+
     return response.data;
   } catch (error) {
     alert('Sorry, the city name you entered is invalid. Please try again.');
     throw error;
   }
-}
+};
