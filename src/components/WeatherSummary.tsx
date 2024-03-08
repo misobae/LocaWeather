5a8d102aeeeb3cfd886d2250ffd97e4408f09757
misobae <misobae0731@gmail.com>
2024-03-08 23:36:35 +0900

    feat: 도시명 함수 fetch 및 적용

--- src/components/WeatherSummary.tsx
+++ src/components/WeatherSummary.tsx
@@ -1,10 +1,12 @@
-import { useMemo } from "react";
-
-import WeatherImg from "./WeatherImg";
-import SearchBox from "./SearchBox";
+import { useEffect, useMemo, useState } from "react";
 
 import { useRecoilValue } from "recoil";
 import { weatherDataState } from "../state/atoms/weatherState";
+
+import { getCityName } from "../api/weatherApi";
+
+import WeatherImg from "./WeatherImg";
+import SearchBox from "./SearchBox";
 
 import {
   Wrap,
@@ -18,6 +20,20 @@ import {
 
 function WeatherSummary() {
   const weatherData = useRecoilValue(weatherDataState);
+  const [cityName, setCityName] = useState('');
+  
+  // 도시 이름 가져오기
+  useEffect(() => {
+    if (weatherData?.lat && weatherData?.lon) {
+      getCityName(weatherData.lat, weatherData.lon)
+        .then(cityName => {
+          setCityName(cityName);
+        })
+        .catch(error => {
+          console.error('도시 이름을 가져오는 중 오류가 발생했습니다:', error);
+        });
+    }
+  }, [weatherData]);
 
   const daysOfWeek = ['SUNDAY', 'MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY'];
   
@@ -30,25 +46,25 @@ function WeatherSummary() {
         <>
           <SearchBox />
           <WeatherImgWrap>
-            <WeatherImg conditionCode={weatherData.weather[0].id} />
+            <WeatherImg conditionCode={weatherData.current.weather[0].id} />
           </WeatherImgWrap>
           <CurrentTemp>
-            <strong>{Math.round(weatherData.main.temp)}</strong>
+            <strong>{Math.round(weatherData.current.temp)}</strong>
             <span>°</span>
           </CurrentTemp>
           <CurrentDate>{daysOfWeek[currentDayOfWeek]}</CurrentDate>
-          <CurrentCity>{weatherData.name}</CurrentCity>
+          <CurrentCity>{cityName}</CurrentCity>
           <CurrentInfo>
             <CurrentInfoItem>
               <img
-                src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`}
-                alt={weatherData.weather[0].description}
+                src={`https://openweathermap.org/img/wn/${weatherData.current.weather[0].icon}.png`}
+                alt={weatherData.current.weather[0].description}
               />
-              {weatherData.weather[0].description}
+              {weatherData.current.weather[0].description}
             </CurrentInfoItem>
             <CurrentInfoItem>
               <img className="icon--feelslike" src={process.env.PUBLIC_URL + '/images/icon_feelslike.svg'} alt="wind chll icon" />
-              Feels like {Math.round(weatherData.main.feels_like)}°
+              Feels like {Math.round(weatherData.current.feels_like)}°
             </CurrentInfoItem>
           </CurrentInfo> 
         </>
