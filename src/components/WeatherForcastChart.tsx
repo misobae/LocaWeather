b9492cfdc3a7212708adb66a2674dc7329236180
misobae <misobae0731@gmail.com>
2024-03-09 16:49:51 +0900

    feat: 주간 날씨 예보 차트 구현

---
+++ src/components/WeatherForcastChart.tsx
@@ -0,0 +1,73 @@
+import { useRecoilValue } from "recoil";
+import { weatherDataState } from "../state/atoms/weatherState";
+
+import ApexCharts from "react-apexcharts";
+
+function ForcastChart() {
+  const weatherData = useRecoilValue(weatherDataState);
+  const weeklyForcastData = weatherData?.daily;
+  const weeklyTemp = weeklyForcastData?.map((day) => {
+    return day.temp.day
+  });
+
+  const weeklyDayNames = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+  const weeklyDay = weeklyForcastData?.map((day) => {
+    const date = new Date(day.dt * 1000).getDay();
+    return weeklyDayNames[date]; // 각 숫자에 해당하는 요일 문자열 반환
+  });
+
+
+  return (
+    <ApexCharts
+      type="area"
+      height={200}
+      options={{
+        chart: {
+          background: "transparent",
+          toolbar: { show: false },
+          zoom: {
+            enabled: false
+          }
+        },
+        dataLabels: {
+          formatter: function(value) {
+            return Number(value).toFixed(0) + "°";
+          },
+          offsetX: 1.5,
+          background: {
+            padding: 8 ,
+            borderRadius: 12,
+            borderWidth: 2,
+          },
+        },
+        colors: ["#333"],
+        stroke: {
+          curve: "smooth",
+          width: 4
+        },
+        xaxis: {
+          categories: weeklyDay,
+        },
+        yaxis: {
+          show: false,
+        },
+        grid: {
+          yaxis: {
+            lines: {
+              show: false
+            }
+          }
+        },
+        tooltip: {
+          enabled: false,
+        }    
+      }}
+      series={[{
+        name: "Temperature",
+        data: weeklyTemp ?? [],
+      }]}
+    />
+  )
+}
+
+export default ForcastChart;
