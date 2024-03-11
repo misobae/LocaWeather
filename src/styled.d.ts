a84fcd0eb6d6b5b6cf02893237e436f00aabd8ad
misobae <misobae0731@gmail.com>
2024-03-11 13:10:06 +0900

    feat: screenSize 삭제

--- src/styled.d.ts
+++ src/styled.d.ts
@@ -2,11 +2,6 @@ import "styled-components";
 
 declare module "styled-components" {
   export interface DefaultTheme {
-    screenSize: {
-      mobile: string;
-      tablet: string;
-      pc: string;
-    };
     color: {
       blk: string;
       gry: string;
